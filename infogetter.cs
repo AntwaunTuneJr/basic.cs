b7772d1ca07411678be19dcb1277209088640a48
Heark <antwauntune25@wcpsstudent.org>
2016-05-23 14:49:54 -0400

    Update infogetter.cs

--- infogetter.cs
+++ infogetter.cs
@@ -1,1 +1,25 @@
 // Get user info
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace infogetter
+{
+    class Gather
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("What is your name?");
+        string user_name = Console.ReadLine();
+        
+        Console.WriteLine("And where are you from?");
+        string from = Console.ReadLine();
+        
+        Console.WriteLine("And how old are you?");
+        string age_str = Console.ReadLine();
+        
+        }
+    }
+}
